4801c24e76c2ab69469aed3aa814d90d700ff359
isdpfhjiuas8ver8asd <antekp05@gmail.com>
2025-04-30 13:08:32 +0200

    ..

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include "json.hpp"
+
+using namespace std;
+using json = nlohmann::json;
+
+int main(int argc, char** argv) {
+	return 0;
+}
