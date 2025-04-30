126f5387ab7d50765a61dcb56cd4097d4eb585ea
isdpfhjiuas8ver8asd <antekp05@gmail.com>
2025-04-30 23:55:29 +0200

    .

--- main.cpp
+++ main.cpp
@@ -6,6 +6,40 @@
 using namespace std;
 using json = nlohmann::json;
 
+
+
+class Konto{};
+class Karta{};
+class Lokata{}; //tymczasowo tutaj zeby kompilator dzialal
+
+
+/**
+ * @class Klient
+ * @brief Reprezentuje klienta banku.
+ * 
+ * 
+ *  Klasa przechowuje dane osobowe oraz udostêpnia podstawowe funkcje zwi¹zane z u¿ytkownikiem 
+ */
+class Klient
+{
+private:
+	string imie; ///< Imiê klienta
+	string nazwisko; ///< Nazwisko klienta
+	string pesel; ///< Numer PESEL klienta
+	string login; ///< Login klienta
+	string haslo; ///< Has³o klienta
+
+	vector<Konto> kontaUzytkownika; ///< Tablica przechowuj¹ca konta u¿ytkownika
+	vector<Karta> kartyUzytkownika; ///< Tablica przechopwuj¹ca karty u¿ytkownika
+	vector<Lokata> lokatyUzytkownika; ///< Tablica przechowuj¹ca lokaty u¿ytkownika
+};
+
+
+
+
+
+
+
 int main(int argc, char** argv) {
 	return 0;
 }
