4bb9a670337fd9e85e8813fccca849669f47dbf2
Eclipse <133004807+HlucaszH@users.noreply.github.com>
2025-05-01 18:17:03 +0200

    Poprawiona poprawność komentarzy

--- main.cpp
+++ main.cpp
@@ -16,27 +16,27 @@ class Konto{};
  * @brief Reprezentuje karte.
  * 
  * 
- *  Klasa przechowuje podtsawowe dane dane i funkcje zwi�zane z kartami 
+ *  Klasa przechowuje podtsawowe dane dane i funkcje zwiazane z kartami 
  */
 class Karta{
 	private:
 		string numerKarty; ///< Numer karty
-		string dataWaznosci; ///< Data wa�no�ci karty
+		string dataWaznosci; ///< Data waznosci karty
 		string kodCVC; ///< Kod CVC karty
 		string typKarty; ///< Typ karty (np. Debitowa, Kredytowa)
-		float saldoKarty; ///< Saldo karty (je�li dotyczy)
+		float saldoKarty; ///< Saldo karty (jezli dotyczy)
 	public:
 		///< getters i setters do atrybutow karty
 		string getNumerKarty() const { return numerKarty; }	///< Zwraca numer karty
-		string getDataWaznosci() const { return dataWaznosci; } ///< Zwraca dat� wa�no�ci karty
+		string getDataWaznosci() const { return dataWaznosci; } ///< Zwraca date waznosci karty
 		string getKodCVC() const { return kodCVC; } ///< Zwraca kod CVC karty
 		string getTypKarty() const { return typKarty; } ///< Zwraca typ karty (np. Debitowa, Kredytowa)
 		float getSaldoKarty() const { return saldoKarty; } ///< Zwraca saldo karty
 		void setNumerKarty(string numer) { numerKarty = numer; } ///< Ustala numer karty
-		void setDataWaznosci(string data) { dataWaznosci = data; } ///< Ustala dat� wa�no�ci karty
+		void setDataWaznosci(string data) { dataWaznosci = data; } ///< Ustala date waznosci karty
 		void setKodCVC(string kod) { kodCVC = kod; } ///< Ustala kod CVC karty		
 		void setTypKarty(string typ) { typKarty = typ; } ///< Ustala typ karty (np. Debitowa, Kredytowa)
-		void setSaldoKarty(float saldo) { saldoKarty = saldo; } ///< Ustala saldo karty (je�li dotyczy)
+		void setSaldoKarty(float saldo) { saldoKarty = saldo; } ///< Ustala saldo karty (jezli dotyczy)
 	};
 class Lokata{}; //tymczasowo tutaj zeby kompilator dzialal
 
@@ -46,71 +46,71 @@ class Lokata{}; //tymczasowo tutaj zeby kompilator dzialal
  * @brief Reprezentuje klienta banku.
  * 
  * 
- *  Klasa przechowuje dane osobowe oraz udost�pnia podstawowe funkcje zwi�zane z u�ytkownikiem 
+ *  Klasa przechowuje dane osobowe oraz udostepnia podstawowe funkcje zwiazane z uzytkownikiem 
  */
 class Klient
 {
 private:
-	string imie; ///< Imi� klienta
+	string imie; ///< Imie klienta
 	string nazwisko; ///< Nazwisko klienta
 	string pesel; ///< Numer PESEL klienta
 	string login; ///< Login klienta
-	string haslo; ///< Has�o klienta
+	string haslo; ///< Haslo klienta
 
-	vector<Konto> kontaUzytkownika; ///< Tablica przechowuj�ca konta u�ytkownika
-	vector<Karta> kartyUzytkownika; ///< Tablica przechopwuj�ca karty u�ytkownika
-	vector<Lokata> lokatyUzytkownika; ///< Tablica przechowuj�ca lokaty u�ytkownika
+	vector<Konto> kontaUzytkownika; ///< Tablica przechowujaca konta uzytkownika
+	vector<Karta> kartyUzytkownika; ///< Tablica przechowujaca karty uzytkownika
+	vector<Lokata> lokatyUzytkownika; ///< Tablica przechowujaca lokaty uzytkownika
 
 	/**
 	 * @brief Rejestruje nowego klienta.
 	 *
-	 * Funckja rejestruje nowego klienta w systemie, zapisuj�c jego dane do pliku JSON (Imi�, nazwisko, PESEL, login, has�o).
+	 * Funckja rejestruje nowego klienta w systemie, zapisujac jego dane do pliku JSON (Imie, nazwisko, PESEL, login, haslo).
 	 */
 	void rejestracja();
 
 	/**
 	 * @brief Loguje klienta do systemu.
 	 *
-	 * Funkcja loguje klienta do systemu, sprawdzaj�c poprawno�� podanych danych (login, has�o).
+	 * Funkcja loguje klienta do systemu, sprawdzajac poprawnosc podanych danych (login, haslo).
 	 */
 	void logowanie();
 
     /**
      * @brief Edytuje dane klienta.
      *
-     * Funkcja umo�liwia edycj� danych klienta, takich jak imi�, nazwisko, czy PESEL,
-     * zapisuj�c zmiany do pliku JSON.
+     * Funkcja umozliwia edycje danych klienta, takich jak imie, nazwisko, czy PESEL,
+     * zapisujac zmiany do pliku JSON.
      */
     void edytujDane();
 
 	/**
-	* @brief Dodaje nowe konto do listy kont u�ytkownika.
+	* @brief Dodaje nowe konto do listy kont uzytkownika.
 	*
-	* Funkcja umo�liwia dodanie nowego konta do listy kont u�ytkownika.
+	* Funkcja umozliwia dodanie nowego konta do listy kont uzytkownika.
 	* Nowe konto jest przechowywane w wektorze kontaUzytkownika.
 	*/
 	void dodajKonto();
 
 	/**
-	 * @brief Dodaje now� kart� do listy kart u�ytkownika.
+	 * @brief Dodaje nowa karte do listy kart uzytkownika.
 	 *
-	 * Funkcja umo�liwia dodanie nowej karty do listy kart u�ytkownika.
+	 * Funkcja umozliwia dodanie nowej karty do listy kart uzytkownika.
 	 * Nowa karta jest przechowywana w wektorze kartyUzytkownika.
 	 */
 	void dodajKarte();
 
 	/**
-	 * @brief Dodaje now� lokat� do listy lokat u�ytkownika.
+	 * @brief Dodaje nowa lokate do listy lokat uzytkownika.
 	 *
-	 * Funkcja umo�liwia dodanie nowej lokaty do listy lokat u�ytkownika.
+	 * Funkcja umozliwia dodanie nowej lokaty do listy lokat uzytkownika.
 	 * Nowa lokata jest przechowywana w wektorze lokatyUzytkownika.
 	 */
 	void dodajLokate();
 
 	/**
-	 * @brief Wy�wietla dane klienta.
+	 * @brief Wyswietla dane klienta.
 	 *
-	 * Funkcja wy�wietla dane klienta, takie jak imi�, nazwisko, PESEL, login i has�o.
+	 * Funkcja wyswietla dane klienta, takie jak imie, nazwisko, PESEL, login i haslo.
 	 */
 	void wyswietlDane();
 };
