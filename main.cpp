daad46398699632ce909a53cfe098ffa904a0d6e
isdpfhjiuas8ver8asd <antekp05@gmail.com>
2025-05-05 23:22:55 +0200

    Transakcja i FileManager

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ using json = nlohmann::json;
  * @brief Reprezentuje karte.
  *
  *
- *  Klasa przechowuje podtsawowe dane dane i funkcje zwiazane z kartami
+ *  Klasa przechowuje podstawowe dane i funkcje zwiazane z kartami
  */
 class Karta {
 private:
@@ -24,6 +24,24 @@ private:
 	string typKarty; ///< Typ karty (np. Debitowa, Kredytowa)
 	float saldoKarty; ///< Saldo karty (jezli dotyczy)
 public:
+	/**
+	 * @brief Konstruktor klasy Karta.
+	 *
+	 * Inicjalizuje obiekt Karta z podanymi parametrami.
+	 *
+	 * @param numer Numer karty
+	 * @param data Data waznosci karty
+	 * @param kod Kod CVC karty
+	 * @param typ Typ karty (np. Debitowa, Kredytowa)
+	 * @param saldo Saldo karty (jesli dotyczy)
+	 */
+	Karta(string numer, string data, string kod, string typ, float saldo) {
+		this->numerKarty = numer;
+		this->dataWaznosci = data;
+		this->kodCVC = kod;
+		this->typKarty = typ;
+		this->saldoKarty = saldo;
+	}
 	/**
 	 * @brief Zwraca numer karty.
 	 *
@@ -51,7 +69,7 @@ public:
 	/**
 	 * @brief Zwraca saldo karty.
 	 *
-	 * @return Saldo karty (jezli dotyczy)
+	 * @return Saldo karty (jesli dotyczy)
 	 */
 	float getSaldoKarty() const { return saldoKarty; }
 	/**
@@ -84,6 +102,7 @@ public:
 	 * @param saldo Saldo karty
 	 */
 	void setSaldoKarty(float saldo) { saldoKarty = saldo; }
+	
 };
 
 
@@ -91,7 +110,7 @@ public:
  * @class Lokata
  * @brief Reprezentuje lokate.
  *
- * Klasa przechowuje kwote, oprocentowanie, date oddania oraz odnosenie do wlasciciela.
+ * Klasa przechowuje kwote, oprocentowanie oraz date oddania.
  */
 class Lokata {
 	float kwota; ///< Kwota lokaty
@@ -114,7 +133,30 @@ public:
 		this->oprocentowanie = oprocentowanie;
 		this->dataOddania = dataOddania;
 	}
-
+	/**
+	 * @brief Konstruktor domyslny klasy Lokata.
+	 *
+	 * Inicjalizuje obiekt Lokata z domyslnymi wartosciami.
+	 */
+	Lokata() = default;
+	/**
+	 * @brief Ustala kwote lokaty.
+	 *
+	 * @param kwota Kwota lokaty
+	 */
+	void setKwota(float kwota) { this->kwota = kwota; }
+	/**
+	 * @brief Ustala oprocentowanie lokaty.
+	 *
+	 * @param oprocentowanie Oprocentowanie lokaty
+	 */
+	void setOprocentowanie(float oprocentowanie) { this->oprocentowanie = oprocentowanie; }
+	/**
+	 * @brief Ustala date oddania lokaty.
+	 *
+	 * @param dataOddania Data oddania lokaty
+	 */
+	void setDataOddania(const string& dataOddania) { this->dataOddania = dataOddania; }
 	/**
 	 * @brief Zwraca kwote lokaty.
 	 *
@@ -135,6 +177,7 @@ public:
 	 * @return Data oddania lokaty
 	 */
 	const string& getDataOddania() const { return dataOddania; }
+	
 };
 
 /**
@@ -145,50 +188,142 @@ public:
  *  Klasa przechowuje podtsawowe dane dane i funkcje zwiazane z kontami 
  */
 class Konto {
-	private:
-		string numerKonta; ///< Numer konta
-		string typKonta; ///< Typ konta (np. Osobiste, Oszczędnościowe)
-		float saldoKonta; ///< Saldo konta
+private:
+	string numerKonta; ///< Numer konta
+	string typKonta; ///< Typ konta (np. Osobiste, Oszczędnościowe)
+	float saldoKonta; ///< Saldo konta
 
-	public:
-		/**
-		 * @brief Zwraca numer konta.
-		 *
-		 * @return Numer konta
-		 */
-		string getNumerKonta() const { return numerKonta; }
-		/**
-		 * @brief Zwraca typ konta.
-		 *
-		 * @return Typ konta (np. Osobiste, Oszczędnościowe)
-		 */
-		string getTypKonta() const { return typKonta; }
-		/**
-		 * @brief Zwraca saldo konta.
-		 *
-		 * @return Saldo konta
-		 */
-		float getSaldoKonta() const { return saldoKonta; }
-		/**
-		 * @brief Ustala numer konta.
-		 *
-		 * @param numer Numer konta
-		 */
-		void setNumerKonta(string numer) { numerKonta = numer; }
-		/**
-		 * @brief Ustala typ konta.
-		 *
-		 * @param typ Typ konta (np. Osobiste, Oszczędnościowe)
-		 */
-		void setTypKonta(string typ) { typKonta = typ; }
-		/**
-		 * @brief Ustala saldo konta.
-		 *
-		 * @param saldo Saldo konta
-		 */
-		void setSaldoKonta(float saldo) { saldoKonta = saldo; }
+public:
+	/**
+	 * @brief Konstruktor klasy Konto.
+	 *
+	 * Inicjalizuje obiekt Konto z podanymi parametrami.
+	 *
+	 * @param numer Numer konta
+	 * @param typ Typ konta (np. Osobiste, Oszczędnościowe)
+	 * @param saldo Saldo konta
+	 */
+	Konto(string numer, string typ, float saldo) {
+			this->numerKonta = numer;
+			this->typKonta = typ;
+			this->saldoKonta = saldo;
+	}
+	/**
+	 * @brief Konstruktor domyslny klasy Konto.
+	 *
+	 * Inicjalizuje obiekt Konto z domyslnymi wartosciami.
+	 */
+	Konto() = default;
+	/**
+	 * @brief Zwraca numer konta.
+	 *
+	 * @return Numer konta
+	 */
+	string getNumerKonta() const { return numerKonta; }
+	/**
+	 * @brief Zwraca typ konta.
+	 *
+	 * @return Typ konta (np. Osobiste, Oszczędnościowe)
+	 */
+	string getTypKonta() const { return typKonta; }
+	/**
+	 * @brief Zwraca saldo konta.
+	 *
+	 * @return Saldo konta
+	 */
+	float getSaldoKonta() const { return saldoKonta; }
+	/**
+	 * @brief Ustala numer konta.
+	 *
+	 * @param numer Numer konta
+	 */
+	void setNumerKonta(string numer) { numerKonta = numer; }
+	/**
+	 * @brief Ustala typ konta.
+	 *
+	 * @param typ Typ konta (np. Osobiste, Oszczędnościowe)
+	 */
+	void setTypKonta(string typ) { typKonta = typ; }
+	/**
+	 * @brief Ustala saldo konta.
+	 *
+	 * @param saldo Saldo konta
+	 */
+	void setSaldoKonta(float saldo) { saldoKonta = saldo; }
+
 };
 
+/**
+ * @class Transakcja
+ * @brief Reprezentuje transakcje.
+ *
+ * Klasa przechowuje dane o transakcji, takie jak kwota, data, typ transakcji oraz zaangażowane konto.
+ */
+class Transakcja
+{
+private:
+	float kwota; ///< Kwota transakcji
+	string typTransakcji; ///< Typ transakcji (np. Wpłata, wypłata, przelew)
+	string dataTransakcji; ///< Data transakcji
+	string kontoNadawcy; ///< Numer konta nadawcy
+	string kontoOdbiorcy; ///< Numer konta odbiorcy
+public:
+	/**
+	 * @brief Domyslny konstruktor klasy Transakcja.
+	 *
+	 * Inicjalizuje obiekt Transakcja z domyslnymi wartosciami.
+	 */
+	Transakcja() = default;
+	/**
+	* @brief Zwraca bieżącą datę w formacie "DD-MM-YYYY".
+	*
+	* @return Bieżąca data w formacie "DD-MM-YYYY"
+	*/
+	string getAktualnaData();
+	/**
+	 * @brief Zwraca kwote transakcji.
+	 *
+	 * @return Kwota transakcji
+	 */
+	float getKwota() const { return kwota; }
+	/**
+	 * @brief Zwraca typ transakcji.
+	 *
+	 * @return Typ transakcji (np. Wpłata, wypłata, przelew)
+	 */
+	string getTypTransakcji() const { return typTransakcji; }
+	/**
+	 * @brief Zwraca date transakcji.
+	 *
+	 * @return Data transakcji
+	 */
+	string getDataTransakcji() const { return dataTransakcji; }
+	/**
+	 * @brief Zwraca numer konta nadawcy.
+	 *
+	 * @return Numer konta nadawcy
+	 */
+	string getKontoNadawcy() const { return kontoNadawcy; }
+	/**
+	 * @brief Zwraca numer konta odbiorcy.
+	 *
+	 * @return Numer konta odbiorcy
+	 */
+	string getKontoOdbiorcy() const { return kontoOdbiorcy; }
+	/**
+	 * @ brief Ustala szczegóły transakcji.
+	 *
+	 * Funckja ustala szczegóły transakcji, takie jak kwota, typ transakcji, data transakcji oraz numery kont nadawcy i odbiorcy.
+	 */
+	void szczegolyTransakcji();
+	/**
+	 * @brief Wyświetla szczegóły transakcji.
+	 *
+	 * Funckja wyświetla szczegóły transakcji, takie jak kwota, typ transakcji, data transakcji oraz numery kont nadawcy i odbiorcy.
+	 */
+	void wyswietlSzczegolyTransakcji();
+	
+};
 
 /**
  * @class Klient
@@ -211,11 +346,76 @@ private:
 	vector<Lokata> lokatyUzytkownika; ///< Tablica przechowujaca lokaty uzytkownika
 
 public:
-
+	/**
+	 *@brief Domyslny konstruktor klasy Klient
+	 *
+	 * Inicjalizuje obiekt Klient z domyslnymi wartosciami.
+	 */
+	Klient() = default;
+	/**
+	 *@brief Ustala imie klienta.
+	 *
+	 *@param imie Imie klienta
+	 */
+	void setImie(string imie) { this->imie = imie; }
+	/**
+	 * @brief Ustala nazwisko klienta.
+	 *
+	 * @param nazwisko Nazwisko klienta
+	 */
+	void setNazwisko(string nazwisko) { this->nazwisko = nazwisko; }
+	/**
+	 * @brief Ustala PESEL klienta.
+	 *
+	 * @param pesel Numer PESEL klienta
+	 */
+	void setPesel(string pesel) { this->pesel = pesel; }
+	/**
+	 * @brief Ustala login klienta.
+	 *
+	 * @param login Login klienta
+	 */
+	void setLogin(string login) { this->login = login; }
+	/**
+	 * @brief Ustala haslo klienta.
+	 *
+	 * @param haslo Haslo klienta
+	 */
+	void setHaslo(string haslo) { this->haslo = haslo; }
+	/**
+	 * @brief Zwraca imie klienta.
+	 *
+	 * @return Imie klienta
+	 */
+	string getImie() const { return imie; }
+	/**
+	 * @brief Zwraca nazwisko klienta.
+	 *
+	 * @return Nazwisko klienta
+	 */
+	string getNazwisko() const { return nazwisko; }
+	/**
+	 * @brief Zwraca PESEL klienta.
+	 *
+	 * @return Numer PESEL klienta
+	 */
+	string getPesel() const { return pesel; }
+	/**
+	 * @brief Zwraca login klienta.
+	 *
+	 * @return Login klienta
+	 */
+	string getLogin() const { return login; }
+	/**
+	 * @brief Zwraca haslo klienta.
+	 *
+	 * @return Haslo klienta
+	 */
+	string getHaslo() const { return haslo; }
 	/**
 	 * @brief Rejestruje nowego klienta.
 	 *
-	 * Funckja rejestruje nowego klienta w systemie, zapisujac jego dane do pliku JSON (Imie, nazwisko, PESEL, login, haslo).
+	 * Funkcja rejestruje nowego klienta w systemie, zapisujac jego dane do pliku JSON (Imie, nazwisko, PESEL, login, haslo).
 	 */
 	void rejestracja();
 
@@ -264,8 +464,162 @@ public:
 	 * Funkcja wyswietla dane klienta, takie jak imie, nazwisko, PESEL, login i haslo.
 	 */
 	void wyswietlDane();
+	
 };
+/**
+ * @class FileManager
+ * @brief Klasa do zarządzania plikami.
+ *
+ * Klasa FileManager jest odpowiedzialna za operacje na plikach, takie jak odczyt i zapis danych.
+ */
+class FileManager
+{
+private:
+	string nazwaPliku; ///< Nazwa pliku do odczytu/zapisu
+	/**
+	 * @brief Zwraca informacje o kliencie w formacie JSON.
+	 *
+	 * Ta funkcja konwertuje obiekt Klient na format JSON, aby ułatwić zapis do pliku.
+	 *
+	 * @param j Obiekt JSON, do którego zostaną zapisane informacje o kliencie
+	 * @param klient Obiekt Klient, którego informacje mają zostać zapisane
+	 */
+	friend void to_json_Klient(json& j, const Klient& klient);
+	/**
+	 * @brief Wczytuje informacje o kliencie z formatu JSON.
+	 *
+	 * Ta funkcja konwertuje dane w formacie JSON na obiekt Klient.
+	 *
+	 * @param j Obiekt JSON, z którego zostaną odczytane informacje o kliencie
+	 * @param klient Obiekt Klient, do którego zostaną zapisane informacje
+	 */
+	friend void from_json_Klient(const json& j, Klient& klient);
+	/**
+	 * @brief Zwraca informacje o transakcji w formacie JSON.
+	 *
+	 * Ta funkcja konwertuje obiekt Transakcja na format JSON, aby ułatwić zapis do pliku.
+	 *
+	 * @param j Obiekt JSON, do którego zostaną zapisane informacje o transakcji
+	 * @param transakcja Obiekt Transakcja, którego informacje mają zostać zapisane
+	 */
+	friend void to_json_Transakcja(json& j, const Transakcja& transakcja);
+	/**
+	 * @brief Wczytuje informacje o transakcji z formatu JSON.
+	 *
+	 * Ta funkcja konwertuje dane w formacie JSON na obiekt Transakcja.
+	 *
+	 * @param j Obiekt JSON, z którego zostaną odczytane informacje o transakcji
+	 * @param transakcja Obiekt Transakcja, do którego zostaną zapisane informacje
+	 */
+	friend void from_json_Transakcja(const json& j, Transakcja& transakcja);
+	/**
+	 * @brief Zwraca informacje o karcie w formacie JSON.
+	 *
+	 * Ta funkcja konwertuje obiekt Karta na format JSON, aby ułatwić zapis do pliku.
+	 *
+	 * @param j Obiekt JSON, do którego zostaną zapisane informacje o karcie
+	 * @param karta Obiekt Karta, którego informacje mają zostać zapisane
+	 */
+	friend void to_json_Karta(json& j, const Karta& karta);
+	/**
+	 * @brief Wczytuje informacje o karcie z formatu JSON.
+	 *
+	 * Ta funkcja konwertuje dane w formacie JSON na obiekt Karta.
+	 *
+	 * @param j Obiekt JSON, z którego zostaną odczytane informacje o karcie
+	 * @param karta Obiekt Karta, do którego zostaną zapisane informacje
+	 */
+	friend void from_json_Karta(const json& j, Karta& karta);
+	/**
+	 * @brief Zwraca informacje o lokacie w formacie JSON.
+	 *
+	 * Ta funkcja konwertuje obiekt Lokata na format JSON, aby ułatwić zapis do pliku.
+	 *
+	 * @param j Obiekt JSON, do którego zostaną zapisane informacje o lokacie
+	 * @param lokata Obiekt Lokata, którego informacje mają zostać zapisane
+	 */
+	friend void to_json_Lokata(json& j, const Lokata& lokata);
+	/**
+	 * @brief Wczytuje informacje o lokacie z formatu JSON.
+	 *
+	 * Ta funkcja konwertuje dane w formacie JSON na obiekt Lokata.
+	 *
+	 * @param j Obiekt JSON, z którego zostaną odczytane informacje o lokacie
+	 * @param lokata Obiekt Lokata, do którego zostaną zapisane informacje
+	 */
+	friend void from_json_Lokata(const json& j, Lokata& lokata);
+	/**
+	 * @brief Zwraca informacje o koncie w formacie JSON.
+	 *
+	 * Ta funkcja konwertuje obiekt Konto na format JSON, aby ułatwić zapis do pliku.
+	 *
+	 * @param j Obiekt JSON, do którego zostaną zapisane informacje o koncie
+	 * @param konto Obiekt Konto, którego informacje mają zostać zapisane
+	 */
+	friend void to_json_Konto(json& j, const Konto& konto);
+	/**
+	 * @brief Wczytuje informacje o koncie z formatu JSON.
+	 *
+	 * Ta funkcja konwertuje dane w formacie JSON na obiekt Konto.
+	 *
+	 * @param j Obiekt JSON, z którego zostaną odczytane informacje o koncie
+	 * @param konto Obiekt Konto, do którego zostaną zapisane informacje
+	 */
+	friend void from_json_Konto(const json& j, Konto& konto);
 
+public:
+	/**
+	 * @brief Zapisuje dane klientów do pliku JSON.
+	 * @param klienci Wektor klientów do zapisania
+	 */
+	void zapiszKlientow(const vector<Klient>& klienci);
+	/**
+	 * @brief Odczytuje dane klientów z pliku JSON.
+	 * @return Wektor klientów odczytanych z pliku
+	 */
+	vector<Klient> wczytajKlientow();
+	/**
+	 * @brief Zapisuje historię transakcji do pliku JSON.
+	 * @param transakcje Wektor transakcji do zapisania
+	 */
+	void zapiszTransakcje(const vector<Transakcja>& transakcje);
+	/**
+	 * @brief Wczytuje historię transakcji z pliku JSON.
+	 * @return Wektor transakcji odczytanych z pliku
+	 */
+	vector<Transakcja> wczytajTransakcje();
+	/**
+	 * @brief Zapisuje dane kart do pliku JSON.
+	 * @param karty Wektor kart do zapisania
+	 */
+	void zapiszKarty(const vector<Karta*>& karty);
+	/**
+	 * @brief Wczytuje dane kart z pliku JSON.
+	 * @return Wektor kart odczytanych z pliku
+	 */
+	vector<Karta*> wczytajKarty();
+	/**
+	 * @brief Zapisuje dane lokat do pliku JSON.
+	 * @param lokaty Wektor lokat do zapisania
+	 */
+	void zapiszLokaty(const vector<Lokata>& lokaty);
+	/**
+	 * @brief Wczytuje dane lokat z pliku JSON.
+	 * @return Wektor lokat odczytanych z pliku
+	 */
+	vector<Lokata> wczytajLokaty();
+	/**
+	 * @brief Zapisuje dane kont do pliku JSON.
+	 * @param konta Wektor kont do zapisania
+	 */
+	void zapiszKonta(const vector<Konto*>& konta);
+	/**
+	 * @brief Wczytuje dane kont z pliku JSON.
+	 * @return Wektor kont odczytanych z pliku
+	 */
+	vector<Konto*> wczytajKonta();
+
+};
 
 
 
